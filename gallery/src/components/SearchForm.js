c4f340d24a9d478dbc93fa091ebd268f671a9e36
ramoshe <rsherard@mac.com>
2021-07-25 03:28:11 -0400

    Update SearchForm.js

--- gallery/src/components/SearchForm.js
+++ gallery/src/components/SearchForm.js
@@ -1,5 +1,5 @@
 const SearchForm = () =>
-    <form class="search-form">
+    <form className="search-form">
         <input type="search" name="search" placeholder="Search" required />
         <button type="submit" className="search-button">
             <svg fill="#fff" height="24" viewBox="0 0 23 23" width="24" xmlns="http://www.w3.org/2000/svg">
